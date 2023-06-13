2efa79e07585b6c6ae193f557d255fce09d3464e
the-albertox <usr3562@salesianos-lacuesta.net>
2023-06-13 10:02:53 +0100

    feat: added actividad2.txt and main class

---
+++ src/activiadad2/Main.java
@@ -0,0 +1,9 @@
+package activiadad2;
+
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+        File activiadad2 = new File("src\activiadad2\activiada2.txt");
+    }
+}
