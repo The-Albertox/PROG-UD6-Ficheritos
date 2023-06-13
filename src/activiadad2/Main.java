8877fb037b4edb455b5f1bf6adf30c5cdeac5dd6
the-albertox <usr3562@salesianos-lacuesta.net>
2023-06-13 10:36:47 +0100

    feat: finish main class

--- src/activiadad2/Main.java
+++ src/activiadad2/Main.java
@@ -1,9 +1,23 @@
 package activiadad2;
-
+ import java.io.BufferedReader;
 import java.io.File;
-
+import java.io.FileReader;
+import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
-        File activiadad2 = new File("src\activiadad2\activiada2.txt");
+        File fichero = new File( "src/activiadad2/fichero2.txt" );
+        StringBuilder contructor = new StringBuilder();
+        try (FileReader lector = new FileReader(fichero);
+             BufferedReader bufflector = new BufferedReader(lector)) {
+           int c;
+            while ((c = bufflector.read()) != -1){
+                contructor.append(c).append("_").append(", ");
+            }
     }
+    catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(contructor.toString());
+}
+
 }
